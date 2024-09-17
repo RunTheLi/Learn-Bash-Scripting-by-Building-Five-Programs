bac1b5608c643c30ffbeecd6bfbb5060d1f4d219
RunTheLi <lanmarock@gmail.com>
2024-09-17 14:10:04 +0700

    add if elif else and numbers

--- bingo.sh
+++ bingo.sh
@@ -4,11 +4,21 @@
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-NUMBER=$RANDOM%75
-echo $NUMBER
+NUMBER=$(( RANDOM % 75 + 1 ))
+TEXT="The next number is, "
 
-
-
-
-
-#J=$(( I - 6 ))
+if (( NUMBER <= 15 ))
+then 
+  echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then 
+  echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
+elif [[ $NUMBER -lt 61 ]]
+then
+  echo $TEXT G:$NUMBER
+else 
+  echo $TEXT O:$NUMBER
+fi
